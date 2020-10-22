8db66ce132b46104033af9bdd40b384e19a75c8e
yuliyambg <yuliyambg@gmail.com>
2020-10-21 23:58:46 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,17 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+  get "/new" do
+    Dog.new("doggy", "colly", 3)
+  end
+
+  get "/dogs" do
+    @dogs = Dog.all
+    erb :dogs
+  end
+
+  get "/dogs_count" do
+    @dog = Dog.count
+    erb :dogs_count
+  end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -12,4 +12,8 @@ class Dog
   def self.all
     @@all
   end
+
+  def self.count
+    @@all.count
+  end
 end
